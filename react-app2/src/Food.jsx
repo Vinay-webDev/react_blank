c3c66109027b6bf304c751c34bcd04b554383e27
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-04-07 15:46:37 +0530

    Food component😋

---
+++ react-app2/src/Food.jsx
@@ -0,0 +1,29 @@
+
+
+function Food() {
+    const food1 = "noodles";
+    const food2 = "pizza";
+
+    return(
+        <div>
+            <h1> my favorite foods😋 </h1>
+            <ul>
+                <li> burger </li>
+                <li> {food1} </li>
+                <li> {food2} </li>
+            </ul>
+        </div>
+    );
+}
+
+export default Food
+
+
+
+
+
+
+
+
+
+
